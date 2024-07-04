e787662752de3f2a3a06f5210efeba9794eff931
joshnice <joshliamnice@gmail.com>
2024-07-04 09:19:19 +0100

    Get current tab

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import "./style.css";
-import viteLogo from "/vite.svg";
-import typescriptLogo from "./typescript.svg";
+import { getCurrentTab } from "./extension/tabs";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 
 if (app == null) {
-  throw new Error("#app not found");
+	throw new Error("#app not found");
 }
 
 app.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    hello world
   </div>
 `;
+
+async function main() {
+	const tab = await getCurrentTab();
+	console.log("tab", tab);
+}
+
+main();
